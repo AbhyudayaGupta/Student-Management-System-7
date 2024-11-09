2aba0f54d13ab12e701a082fd3606fe61854addd
Abhyudaya Gupta <146164426+AbhyudayaGupta@users.noreply.github.com>
2024-11-09 15:35:52 +0530

    Student Management System

---
+++ student/app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','user');
+define('DB_PASS','');
+define('DB_NAME','sms_db');
+
+define('APPROOT',dirname(dirname(__FILE__)));
+
+define('URLROOT','http://localhost/student');
+
+define('SITENAME','SMS SYSTEM');
